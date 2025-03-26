305f5269f5f8ddb37b7b5e3e0782a835986c9cd6
LOGO1012 <ekthf988173@gmail.com>
2025-03-26 16:26:18 +0900

    chore: layout add

--- src/pages/ProductShowcasePage.jsx
+++ src/pages/ProductShowcasePage.jsx
@@ -1,13 +1,16 @@
 // src/pages/ProductShowcasePage.jsx
 import React from 'react';
 import ProductList from '../components/product/UserProductList.jsx';
+import BasicLayout from "../layout/BasicLayout.jsx";
 
 const ProductShowcasePage = () => {
     return (
+        <BasicLayout>
         <div className="min-h-screen p-6 bg-gray-100">
             <h1 className="text-3xl font-bold mb-6 text-center">상품 진열 페이지</h1>
             <ProductList />
         </div>
+        </BasicLayout>
     );
 };
 
