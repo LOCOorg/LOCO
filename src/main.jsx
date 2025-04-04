613fa273f5540a3fef00504c04b8b82c88f5b65f
LOGO1012 <ekthf988173@gmail.com>
2025-04-04 17:31:58 +0900

    feat: chat notification fn add

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import mainRouter from "./routers/MainRouter.jsx";
 import { RouterProvider } from 'react-router-dom'
-import AuthInit from '../src/components/authComponent/AuthInit.jsx'; // AuthInit import
+import AuthInit from '../src/components/authComponent/AuthInit.jsx';
+import ChatNotification from "./components/chatcomponents/ChatNotification.jsx"; // AuthInit import
 
 createRoot(document.getElementById('root')).render(
     <>
         <AuthInit />
+        <ChatNotification/>
         <RouterProvider router={mainRouter} />
     </>
 )
