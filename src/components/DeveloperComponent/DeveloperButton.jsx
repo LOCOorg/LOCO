5291be38c9278b6e7bd56ef066398de5b3841170
Jeongseok1 <wjdtjr46179702@gmail.com>
2025-04-18 14:47:24 +0900

    개발자 페이지

---
+++ src/components/DeveloperComponent/DeveloperButton.jsx
@@ -0,0 +1,22 @@
+// src/components/ProfileAccessButton.jsx
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const PRButton = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/developer'); // 라우터 등록한 경로
+    };
+
+    return (
+        <button
+            onClick={handleClick}
+            className="mt-4 px-6 py-3 bg-indigo-500 text-white text-lg rounded-lg shadow-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        >
+            관리자 페이지
+        </button>
+    );
+};
+
+export default PRButton;
---
+++ src/routers/DeveloperRouter.jsx
@@ -0,0 +1,9 @@
+// src/routers/MyPageRouter.jsx
+import DeveloperPage from "../pages/DeveloperPage/DeveloperPage.jsx";
+
+const DeveloperRouter = {
+    path: "/developer",
+    element: < DeveloperPage />
+};
+
+export default DeveloperRouter;
