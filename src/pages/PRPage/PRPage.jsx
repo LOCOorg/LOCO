56cf19e9b713a80642572f4a70ddb59dad27b48d
Jeongseok1 <wjdtjr46179702@gmail.com>
2025-05-09 16:13:36 +0900

    pr페이지

--- src/pages/PRPage/PRPage.jsx
+++ src/pages/PRPage/PRPage.jsx
@@ -1,14 +1,14 @@
 import PRPageComponent  from "../../components/PR/PRPageComponent.jsx";
-import BasicLayout from "../../layout/BasicLayout.jsx";
+
 
 const PRPage  = () => {
     return (
-        <BasicLayout>
+
         <div className="p-6">
             <h1 className="text-3xl font-bold mb-6">PR페이지</h1>
             <PRPageComponent />
         </div>
-        </BasicLayout>
+
     );
 };
 
