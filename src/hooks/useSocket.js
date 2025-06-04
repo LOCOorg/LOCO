54e67afafc77862f58faf916b666f4b8f4672f4b
LOGO1012 <ekthf988173@gmail.com>
2025-06-04 18:05:03 +0900

    chore: socket url edit

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL = `${import.meta.env.VITE_API_HOST}`; // 웹소켓 서버 주소
+const SOCKET_SERVER_URL = `${import.meta.env.VITE_API_SOCKET}`; // 웹소켓 서버 주소
 
 export const useSocket = () => {
     const [socket, setSocket] = useState(null);
