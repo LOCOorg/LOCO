c167201fc08aafdecff1bec0d2ed84fc486fe917
LOGO1012 <ekthf988173@gmail.com>
2025-07-11 15:43:44 +0900

    chore: global chat overlay location edit

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,11 @@ import './index.css'
 import mainRouter from "./routers/MainRouter.jsx";
 import { RouterProvider } from 'react-router-dom'
 import AuthInit from '../src/components/authComponent/AuthInit.jsx';
-import GlobalFriendChatOverlay from "./components/chatcomponents/GlobalFriendChatOverlay.jsx";
 import {NotificationProvider} from "./hooks/NotificationContext.jsx";
 
 createRoot(document.getElementById('root')).render(
     <NotificationProvider>
         <AuthInit />
         <RouterProvider router={mainRouter} />
-        <GlobalFriendChatOverlay />
     </NotificationProvider>
 )
