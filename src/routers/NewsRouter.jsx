21048419ce6d7bd6a7b7282e71a5fd7715fd090e
Jeongseok1 <wjdtjr46179702@gmail.com>
2026-02-09 23:28:29 +0900

    유저 인증 보안

--- src/routers/NewsRouter.jsx
+++ src/routers/NewsRouter.jsx
@@ -2,14 +2,29 @@ import NewsPage from "../pages/newsPages/NewsPage.jsx";
 import NewsDetailPage from "../pages/newsPages/NewsDetailPage.jsx";
 import NewsWritePage from "../pages/newsPages/NewsWritePage.jsx";
 import NewsEditPage from "../pages/newsPages/NewsEditPage.jsx";
+import AdminGuard from "../components/authComponent/AdminGuard.jsx";
 
 const NewsRouter = {
     path: "news",
     children: [
-        { index: true, element: <NewsPage /> },
-        { path: "write", element: <NewsWritePage /> },
-        { path: ":id/edit", element: <NewsEditPage /> },
-        { path: ":id", element: <NewsDetailPage /> }
+        { index: true, element: <NewsPage /> },  // 공개
+        {
+            path: "write",
+            element: (
+                <AdminGuard>
+                    <NewsWritePage />
+                </AdminGuard>
+            )
+        },
+        {
+            path: ":id/edit",
+            element: (
+                <AdminGuard>
+                    <NewsEditPage />
+                </AdminGuard>
+            )
+        },
+        { path: ":id", element: <NewsDetailPage /> }  // 공개
     ]
 };
 
