165c946a50ac5e4996550b1cd96ff4c171a6f866
Jeongseok1 <wjdtjr46179702@gmail.com>
2026-03-03 18:01:01 +0900

    instance 적용

--- src/components/DeveloperComponent/chatcomponents/ChatUserSearchPanel.jsx
+++ src/components/DeveloperComponent/chatcomponents/ChatUserSearchPanel.jsx
@@ -1,6 +1,6 @@
 // C:\Users\wjdtj\WebstormProjects\LOCO\src\components\DeveloperComponent\chatcomponents\ChatUserSearchPanel.jsx
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import instance from '../../../api/axiosInstance.js';
 import UserListItem from '../UserListItem.jsx';
 
 const ChatUserSearchPanel = ({ selectedUser, setSelectedUser }) => {
@@ -8,7 +8,7 @@ const ChatUserSearchPanel = ({ selectedUser, setSelectedUser }) => {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        axios
+        instance
             .get(`/api/developer/users?query=${encodeURIComponent(query)}`)
             .then(res => setUsers(res.data.results || []))
             .catch(console.error);
